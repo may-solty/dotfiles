0d8076cd79598ddbd57524fbd9c9720ff91eaf60
takuma shiono <style.lpolvues@gmail.com>
2018-10-20 22:17:42 +0900

    Add : tmux

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -6,3 +6,10 @@ ln -sf ~/dotfiles/.bashrc ~/.bashrc
 
 # editorconfig
 ln -sf ~/dotfiles/.editorconfig ~/.editorconfig
+
+# tmux
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+if [ -d ~/.bash/conf ]; then
+    mkdir -p ~/.bash/conf
+fi
+ln -sf ~/dotfiles/tmux.bash ~/.bash/conf/tmux.bash
