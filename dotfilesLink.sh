ed422c03df2df4bb017f678531162d3b577ba354
T.shiono <t.shiono@id-entity.jp>
2019-05-08 14:22:29 +0900

    Update: symbolic link script

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -7,6 +7,14 @@ ln -sf ~/dotfiles/.bashrc ~/.bashrc
 # editorconfig
 ln -sf ~/dotfiles/.editorconfig ~/.editorconfig
 
+# fish
+ln -sf ~/dotfiles/.config/fish/conf.d/omf.fish ~/.config/fish/conf.d/omf.fish
+
+# Oh My Fish
+ln -sf ~/dotfiles/.config/omf/bundle ~/.config/omf/bundle
+ln -sf ~/dotfiles/.config/omf/channel ~/.config/omf/channel
+ln -sf ~/dotfiles/.config/omf/theme ~/.config/omf/theme
+
 # tmux
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 if [ ! -d ~/.bash/conf ]; then
@@ -16,7 +24,7 @@ ln -sf ~/dotfiles/tmux.bash ~/.bash/conf/tmux.bash
 
 # vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-if [ ! -f ~/.vim/bundle/neobundle.vim ]; then
+if [ ! -d ~/.vim/bundle/neobundle.vim ]; then
     mkdir -p ~/.vim/bundle
     git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 fi
